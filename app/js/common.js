fae88153cc632cd55a4be718c468cba2d1c1977d
Forest_Hub <anatolijk096@gmail.com>
2020-04-20 23:17:46 +0300

    error correction

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,9 @@
 $(".testimonials-section .slider-wrap").slick({
   nextArrow: $(".testimonials-section .next"),
-  prevArrow: $(".testimonials-section .prev")
+  prevArrow: $(".testimonials-section .prev"),
+});
+
+$(".services-section .services-section__slider-wrap").slick({
+  nextArrow: $(".services-section .next"),
+  prevArrow: $(".services-section .prev"),
 });
