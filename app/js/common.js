e74096309b2c3f0e12bae8d795ed94b69131f731
Forest_Hub <anatolijk096@gmail.com>
2020-04-23 14:36:08 +0300

    finished layout

--- app/js/common.js
+++ app/js/common.js
@@ -19,9 +19,22 @@ $(".services-section .services-section__slider-wrap").slick({
   nextArrow: $(".services-section .next"),
   prevArrow: $(".services-section .prev"),
 });
-const mixer = mixitup("#mixitup-container");
+
+if (document.getElementById("mixitup-container")) {
+  mixitup("#mixitup-container");
+}
 
 const menuBtn = document.getElementById("menu-btn");
+const menuWrp = document.getElementById("navigation-menu");
 menuBtn.addEventListener("click", function () {
   menuBtn.classList.toggle("is-open");
+  menuWrp.classList.toggle("menu-opened");
+
+  if (menuBtn.classList.contains("is-open")) {
+    document.body.style.overflow = "hidden";
+    // menuWrp.classList.add("menu-opened");заменяется classList.toggle
+  } else {
+    document.body.style.overflow = "visible";
+    // menuWrp.classList.remove("menu-opened");заменяется classList.toggle
+  }
 });
